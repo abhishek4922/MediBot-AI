80954f9df371a102bbf882daa2376d6665cb6d88
abhishek4922 <abhishekjain18138@gmail.com>
2025-06-27 13:18:36 +0530

    folder setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'medibot',
+    version='0.0.0',
+    author='abhishek',
+    author_email='abhishekjain18138@gmail.com',
+    packages = find_packages(),
+    install_requires=[]
+
+)
